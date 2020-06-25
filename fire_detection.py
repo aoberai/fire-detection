8647e094a7f7889f36d72c45e4ae1696d4d11ac6
aoberai <aoberai05@gmail.com>
2020-06-24 20:05:31 -0700

    increased accuracy using conv2d and max pooling stack. Has trouble differentiating between th e sunrise/sunset/sunlight/sun and fire

--- fire_detection.py
+++ fire_detection.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import cv2
 
+wants_model_save = False
 
 def preprocess_image(preprocessed_image: np.ndarray) -> np.ndarray:
     new_image = cv2.resize(src=preprocessed_image, dsize=(input_shape[0], input_shape[1]))
@@ -20,6 +21,10 @@ one_hot_encoded_array = np.array([1] * fire_images_dataset_length + [
     0] * non_fire_images_dataset_length)  # 1 represents fire, 0 represents no fire
 
 model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (5, 5), input_shape=input_shape),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(128, (3, 3), input_shape=input_shape),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
     tf.keras.layers.Flatten(input_shape=input_shape),
     tf.keras.layers.Dense(512, activation='relu'),
     tf.keras.layers.Dropout(0.2),
@@ -55,9 +60,11 @@ model.fit(x=np.array(fire_detection_train_dataset),
 
 print("\n Model created\n\n")
 model_path = "./fire_detection_model.h5"
-model.save(model_path)
 
-test_image = preprocess_image(cv2.imread("model_test_images/non_fire.3.png"))
+if wants_model_save:
+    model.save(model_path)
+
+test_image = preprocess_image(cv2.imread("model_test_images/non_fire.9.png"))
 test_image = np.expand_dims(test_image, 0)
 
 prediction = model.predict(test_image)
@@ -68,5 +75,4 @@ if prediction[0][0] < prediction[0][1]:
 else:
     print("Image is detected as not fire")
     print("Confidence: {0}".format(prediction[0][0] * 100))
-
 
